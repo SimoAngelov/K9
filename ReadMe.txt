9c580b3e1ccd4f4b99570f68c60fb3d7c18a38de
Simo Angelov <simeon_g_angelov@abv.bg>
2021-05-05 17:15:46 +0300

    [K9]: Hello world

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+K9 is an OpenGL sandbox wrapper, used to test and learn its functionality.
+
+Prerequisites:
+1) Update the system: sudo apt-get update -y
+2) CMake: sudo apt-get install -y cmake
---
+++ build/CMakeFiles/K9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/K9.dir/src/main.cpp.o"
+  "K9"
+  "K9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/K9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
